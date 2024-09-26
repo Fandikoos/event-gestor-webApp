2ef45d4273a0553f525d26e221216a86c4931910
Fandikoos <danfandos@gmail.com>
2024-09-26 12:58:23 +0200

    Add folding structure

---
+++ src/app/components/events/event-list/event-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-list.component.html',
+  styleUrl: './event-list.component.css'
+})
+export class EventListComponent {
+
+}
